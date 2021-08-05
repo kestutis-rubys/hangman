7267244d9cd9c57d7a7428ea319fcf922cf15b42
kestutis-rubys <kestutis,rubys@gmail.com>
2021-08-05 14:26:28 +0300

    added style, showLetters function, new words

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 const randomWordElement = document.querySelector('.randomWord');
 let pointsValue = document.querySelector('.number');
+const buttonElement = document.querySelector('button');
 const answerElement = document.querySelector('.answer');
-const data = ['Football', 'Basketball', 'Tennis', 'Boxing', 'Valleyball'];
+const data = [
+  'Football',
+  'Basketball',
+  'Tennis',
+  'Boxing',
+  'Volleyball',
+  'impress',
+  'deprive',
+  'disappoint',
+  'spokesperson',
+  'admission',
+  'community',
+  'restless',
+  'wind',
+  'barrier',
+  'innovation',
+  'convention',
+  'appointment',
+  'reinforce',
+  'activity',
+];
+const incorrectLetters = document.querySelector('.incorrectLetters');
 const hiddenWord = [];
 let selectedWord = '';
 let points;
 let number = 0;
 let minusPoints = 0;
+const images = ['2.png', '3.png', '4.png', '5.png', '6.png', '7.png'];
+const imgElement = document.querySelector('img');
+let imgIndex = 0;
 
 // Functions
 
@@ -34,6 +59,11 @@ const checkLetter = (letter) => {
       number++;
     }
   });
+  const findLetter = selectedWord.find((item) => item === letter);
+  if (!findLetter) {
+    incorrectLetters.textContent += letter.toUpperCase() + ' ';
+  }
+
   minusPoints = number - selectedWord.length;
   changePointsValue(minusPoints);
 };
@@ -43,16 +73,25 @@ const changePointsValue = (number) => {
     if (number === 0) {
       points--;
       pointsValue.textContent = points;
+      if (imgIndex <= images.length) {
+        imgElement.src = './img/' + images[imgIndex];
+        imgIndex++;
+      }
     }
   } else {
     pointsValue.textContent = 0;
+    imgElement.src = './img/' + images[images.length - 1];
     answerElement.textContent = 'Unfortunately, you are out of lives';
+    answerElement.classList.add('red');
+    buttonElement.classList.add('showBtn');
   }
 };
 
 const checkWord = (hiddenWord) => {
   if (JSON.stringify(selectedWord) === JSON.stringify(hiddenWord)) {
     answerElement.textContent = 'Congratulations, you survived!';
+    answerElement.classList.add('green');
+    buttonElement.classList.add('showBtn');
   }
 };
 
@@ -68,7 +107,10 @@ document.addEventListener('keypress', (e) => {
   minusPoints = 0;
   const keyValue = e.key;
   checkLetter(keyValue);
-  // console.log(hiddenWord);
-  // console.log(selectedWord);
 });
 // Read enter key
+
+buttonElement.addEventListener('click', () => {
+  location.reload();
+  buttonElement.classList.remove('showBtn');
+});
